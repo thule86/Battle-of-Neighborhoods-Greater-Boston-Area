0d123a4d2859f7b8bd502f8ae714862d26057b7a
julie150720 <68930546+julie150720@users.noreply.github.com>
2021-07-24 22:46:03 -0400

    Add files via upload

---
+++ Coursera Capstone.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used for Coursera capstone project
+
+# In[3]:
+
+
+
+import pandas as pd
+import numpy as np
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
